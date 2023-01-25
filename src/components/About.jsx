f8c30fcb40e12139a373710e1405ceb2162aac90
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-01-25 04:53:24 +0000

    css applied

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,10 @@
 export function About() {
     return (
-        <h2>About</h2>
+        <div className="about">
+            <h2>About</h2>
+            <p>An inquisitive pug who is larger than a small pug. Knows an unexpectedly
+                broad array of things. Currently pioneering research into the future of
+                internet plumbing.</p>
+        </div>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,5 +1,10 @@
 export function Interests() {
     return (
-        <h2>Interests</h2>
+        <div className="interests">
+            <h2>Interests</h2>
+            <p>Frontend. Backend. Back of the backend. Hardware for the back of the backend.
+                Connecting the hardware for the back of the backend together… and doing
+                it automatically.</p>
+        </div>
     )
 }
